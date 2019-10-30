c71f8b947034e6434f9e8894b875ff7e0b4d1123
Capitan Pinto <santiagopfisica@gmail.com>
2019-10-30 23:13:41 +0000

    Bulma

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get calories" do
+    get static_pages_calories_url
+    assert_response :success
+  end
+
+end
