7490a3bf36285331b54b2eff2e365fd536daf6da
Capitan Pinto <santiagopfisica@gmail.com>
2019-11-04 04:21:32 +0000

    seeded finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar")
+User.create!(name:  "No Negativo",
+             email: "no@no.no",
+             password:              "nonono",
+             password_confirmation: "nonono")
 
 # Generate a bunch of additional users.
 99.times do |n|
@@ -19,4 +19,20 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password)
+end
+
+now = Time.now
+month_ago = now - 60 * 60 * 24 * 120
+users = User.order(:created_at).take(6)
+a=[0,1,2,3,4,5]
+
+240.times do
+  a.shuffle!
+  content = Faker::Lorem.sentence(word_count: 5)
+  nc=a[1]
+  qc=a[2]
+  rand_time = rand(month_ago..now)
+  users.each { |user| user.calos.create!(coment: content, 
+                                              created_at: rand_time,
+                                              ncal: nc, qcal: qc) }
 end
